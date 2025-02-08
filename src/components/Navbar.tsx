4c7f0dca483692bd2fc83994a2e0e194bd2a8bdb
gomdopi <io.gomdopi@gmail.com>
2025-02-08 11:21:16 -0800

    updates for lesson 24

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,7 @@ export default function Navbar() {
   return (
     <header className="header">
       <nav className="nav">
-        <img
-          src="/public/react-logo.png"
-          alt="React logo"
-          className="nav-logo"
-        />
+        <img src="/react-logo.png" alt="React logo" className="nav-logo" />
         <span className="nav-text">ReactFacts</span>
       </nav>
     </header>
