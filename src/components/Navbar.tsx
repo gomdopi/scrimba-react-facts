c4303c5509e9dff63e58489d776ba07d68329770
gomdopi <io.gomdopi@gmail.com>
2025-02-07 21:09:17 -0800

    updates up to and including lesson 23

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <img
+          src="/public/react-logo.png"
+          alt="React logo"
+          className="nav-logo"
+        />
+        <span className="nav-text">ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,6 @@
 import { createRoot } from "react-dom/client"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
+import App from "./App"
 
 const root = createRoot(document.getElementById("root")!)
 
-function Page() {
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
-  )
-}
-
-root.render(<Page />)
+root.render(<App />)
